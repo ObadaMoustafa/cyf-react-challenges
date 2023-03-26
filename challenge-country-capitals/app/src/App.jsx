240393f1520ac9dcb129e4e08f72e7c32aabe0f1
Obada Moustafa Sharbatly <obadamoustafa@gmail.com>
2023-03-26 16:09:08 +0200

    initalize new project

---
+++ challenge-country-capitals/app/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>country guessing game challenge </h2>
+        <code>{`const number = { name: "obada", age: 32}`}</code>
+        <code>const number =5;</code>
+        <code>const number =5;</code>
+      </header>
+    </div>
+  );
+}
+
+export default App;
